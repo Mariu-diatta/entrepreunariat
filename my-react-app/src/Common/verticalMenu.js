c193d4ec2a37a3b631fa3ed43fa04e88fa97013e
Marius_DIATTA <marius@kayfo.games>
2023-11-25 00:44:00 +0100

    nav vertical done

--- my-react-app/src/Common/verticalMenu.js
+++ my-react-app/src/Common/verticalMenu.js
@@ -1,5 +1,5 @@
 import { useState } from "react"
-
+import { Link } from "react-router-dom"
 const couleurBlue = {backgroundColor:"", color:"blue"}
 const couleurBlack= {backgroundColor:"", color:"black"}
 const couleurWhite = {backgroundColor:"", color:"black"}
@@ -16,41 +16,41 @@ const VerticalMenu=()=>{
     return (
         <div className="col-lg-2 u-grey-10">  
             <nav className=" sticky-top flex-row " style={{zIndex:0, paddingTop:"20px"}}>
-                <h5 style={{paddingLeft:"30px", paddingTop:"10px"}}>Activité</h5>
+                <h5 className="p-4">Activités</h5>
                 <ul className=" sticky-top flex-row ">
 
-                    <li>  <a href="#" onClick={()=>messageActivate?setMessageActivate(false):setMessageActivate(true)} onMouseOver={()=>setCouleurMessage(couleurBlue)} onMouseOut={()=>setCouleurMessage(couleurWhite)} style={couleurMessage}><small>Messages</small></a></li>
+                    <li>  <button className='btn' onClick={()=>messageActivate?setMessageActivate(false):setMessageActivate(true)} onMouseOver={()=>setCouleurMessage(couleurBlue)} onMouseOut={()=>setCouleurMessage(couleurWhite)} style={couleurMessage}>Messages</button></li>
                     { 
                         (messageActivate)?
                         <nav>
                         <ul >
-                            <li> <a href="#" className="btn btn-primary btn-sm m-2" style={{backgroundColor:"yellow", color:"blue"}}>Nouveaux Messages</a></li>
-                            <li> <a href="#" className="btn btn-primary btn-sm m-2">Anciencs Messages</a></li>
+                            <li> <Link to="#" className=" "><small>Nouveaux Messages</small></Link></li>
+                            <li> <Link to="#" className=" "><small>Anciencs Messages</small></Link></li>
                         </ul>
                         </nav>: <nav> </nav>
                 
                     }
 
-                    <li > <a href="#" onClick={()=>adherantActivate?setAdherantActivate(false):setAdherantActivate(true)}  onMouseOver={()=>setcouleurAdherant(couleurBlue)} onMouseOut={()=>setcouleurAdherant(couleurWhite)} style={couleurAdherant}><small>Adherants</small></a></li>
+                    <li > <button className='btn' onClick={()=>adherantActivate?setAdherantActivate(false):setAdherantActivate(true)}  onMouseOver={()=>setcouleurAdherant(couleurBlue)} onMouseOut={()=>setcouleurAdherant(couleurWhite)} style={couleurAdherant}>Adherants</button></li>
                     {  
                         (adherantActivate)?
                         <nav>
                                 <ul >
-                                    <li> <a href="#" className="btn btn-primary btn-sm m-2" style={{backgroundColor:"yellow", color:"blue"}}>Nouveaux Adhérants</a></li>
-                                    <li> <a href="#" className="btn btn-primary btn-sm m-2">Anciens Adhérants</a></li>
+                                    <li> <Link to="#" className=" "><small>Nouveaux Adhérants</small></Link></li>
+                                    <li> <Link to="#" className=" "><small>Anciens Adhérants</small></Link></li>
                                 </ul>
                         </nav>:<nav></nav>
                     }
                     
-                    <li > <a href="#" onClick={()=>commandeActivate?setCommandeActivate(false):setCommandeActivate(true)} onMouseOver={()=>setCouleurCommande(couleurBlue)} onMouseOut={()=>setCouleurCommande(couleurWhite)}  style={couleurCommande}><small>Commandes</small></a></li>
+                    <li > <button className='btn' onClick={()=>commandeActivate?setCommandeActivate(false):setCommandeActivate(true)} onMouseOver={()=>setCouleurCommande(couleurBlue)} onMouseOut={()=>setCouleurCommande(couleurWhite)}  style={couleurCommande}>Commandes</button></li>
                     {                        
                         (commandeActivate)?
                         <nav>
-                            <ul >
-                                <li> <a href="#" className="btn btn-primary btn-sm m-2" style={{backgroundColor:"yellow", color:"blue"}}>Commandes réalisées</a></li>
-                                <li> <a href="#" className="btn btn-primary btn-sm m-2" >Commandes programées</a></li>
-                                <li> <a href="#" className="btn btn-primary btn-sm m-2" style={{backgroundColor:"red", color:"blue"}}>Commandes en cours</a></li>
-                                <li> <a href="#" className="btn btn-primary btn-sm m-2" style={{backgroundColor:"green"}}>Commandes prêtes</a></li>
+                            <ul>
+                                <li> <Link to="#" className=" "><small>Commandes réalisées</small></Link></li>
+                                <li> <Link to="#" className=" "><small>Commandes programées</small></Link></li>
+                                <li> <Link to="#" className=" "><small>Commandes en cours</small></Link></li>
+                                <li> <Link to="#" className=" "><small>Commandes prêtes</small></Link></li>
                             </ul>
                         </nav>: <nav></nav>
                     }
@@ -59,4 +59,5 @@ const VerticalMenu=()=>{
         </div>
     )
 }
+
 export default VerticalMenu;
