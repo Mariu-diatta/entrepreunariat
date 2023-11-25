5164b6fbe7117262960add214c6e57833bdde11a
Marius_DIATTA <marius@kayfo.games>
2023-11-25 14:12:23 +0100

    commande done

--- my-react-app/src/Common/commande.js
+++ my-react-app/src/Common/commande.js
@@ -1,6 +1,15 @@
 import im from "../accueil/Site1/images/3454.jpg";
 import BtnSmt from "./buttonSubmit";
 
+const cars = [
+    {prod:1,  lab:'Ford',qual:'bon'},
+    {prod:1,  lab:'Ford',qual:'bon'},
+    {prod:1,  lab:'Ford',qual:'bon'},
+    {prod:1,  lab:'Ford',qual:'bon'},
+    {prod:1,  lab:'Ford',qual:'bon'},
+    {prod:1,  lab:'Ford',qual:'bon'}
+];
+
 const Commande =()=>{
     return(
         <>
@@ -14,29 +23,50 @@ const Commande =()=>{
                                         </div>
                                     </div>
                                     <div className="u-align-left u-container-align-left u-container-style u-layout-cell u-left-cell u-size-30 u-layout-cell-2" data-animation-name="customAnimationIn" data-animation-duration="1500" data-animation-delay="500">
-                                        <div className="u-container-layout u-valign-top u-container-layout-1">
-                                            <h5 className="u-align-left u-text u-text-1">Disponible</h5>
-                                            <p className="u-align-left u-text u-text-2">Thiré lalo</p>
-                                        </div>
+                                        
+                                            <ul className="row p-2  u-container-layout u-valign-top u-container-layout-1" style={{textAlign:'center'}}>
+                                                <h5 className="u-text u-text-1">Sélectionez vos produits.</h5>
+                                                <form style={{width:'100%', padding:12}}>
+                                                <table>
+                                                    <tr>
+                                                        <th className="p-4">produits</th>
+                                                        <th className="p-4">Quantité</th>
+                                                        <th className="p-4">Qualité</th>
+                                                        <th className="p-4"></th>
+                                                    </tr>
+                                                    {cars.map((car) =><tr><td className="spacing_d"> {car.prod} </td> <td className="spacing_d"> {car.lab} </td> <td className="spacing_d"> {car.qual} </td><td className="spacing_d"> {<input type="checkbox"/>} </td></tr>)}
+                                                </table>
+                                                <BtnSmt/>
+                                                </form>
+                                            </ul>
+                                           
+                                           
+                                        
                                     </div>
                                 </div>
                             </div>
-                            <div className="col-lg-6  " style={{backgroundColor:"grey", marginLeft:"0px"}}>
+                            <div className="col-lg-6  " style={{backgroundColor:"white", marginLeft:"0px"}}>
                                
-                                <div className="row" style={{backgroundColor:"white", textAlign:'center', paddingTop:'15px', paddingBottom:'20px'}}>
-                                    <h5> Enregistrer vos données pour valider l'opération</h5>
+                                <div className="m-1 p-4 row" style={{backgroundColor:"", textAlign:'center', paddingTop:'15px', paddingBottom:'20px'}}>
+                                    <h5> Enregistrer vos données pour valider l'opération.</h5>
                                     <form>
-                                        <label for="fname">Nom:</label><br/>
-                                        <input type="text" id="fname" name="fname"/><br/>
-                                        <label for="lname">Prénom:</label><br/>
-                                        <input type="text" id="lname" name="lname"/><br/>
-                                        <label for="lname">Number:</label><br/>
-                                        <input type="text" id="lname" name="lname"/><br/>
+                                        <div className="u-form-group u-form-name u-form-partition-factor-2 u-label-none u-form-group-1" style={{paddingLeft:'20%', paddingRight:'20%'}}>    
+                                            <input type="text" id="fname" name="fname"  className="u-align-center u-input" maxlength="20" placeholder="numéro"/><br/>
+                                        </div>
+                                        <div className="u-form-group u-form-name u-form-partition-factor-2 u-label-none u-form-group-1" style={{paddingLeft:'20%', paddingRight:'20%'}}>
+                                            <input type="text" id="lname" name="lname" className="u-align-center u-input" maxlength="20" width={""} placeholder="nom" /><br/>
+                                        </div>
+                                        <div className="u-form-group u-form-name u-form-partition-factor-2 u-label-none u-form-group-1" style={{paddingLeft:'20%', paddingRight:'20%'}}>
+                                            <input type="text" id="lname" name="lname" className="u-align-center u-input" maxlength="20" placeholder="numéro"/><br/>
+                                        </div>
+
+                                        <div className="row" style={{backgroundColor:"grey", textAlign:'center', paddingTop:'12px'}}>
+                                            <h6> Résumé de votre commande </h6>
+                                            {cars.map((car) => <p key={car.id}>{car.brand} </p>)}
+                                        </div>
+
                                         <BtnSmt/>
                                     </form> 
-                                </div>
-                                <div className="row" style={{backgroundColor:"white", textAlign:'center', paddingTop:'12px'}}>
-                                    <h6> Résumé de votre commande </h6>
                                 </div>
                          
                             </div>
