c22aa8e0d7a1a3be4ddeb6fc622795bc814db015
Marius_DIATTA <marius@kayfo.games>
2023-12-01 10:30:50 +0100

    merge

--- my-react-app/src/Common/sectionU.js
+++ my-react-app/src/Common/sectionU.js
@@ -15,20 +15,17 @@ const Data=[
 
 function SectionU(){
   return(
-      <section  className="container  u-align-center u-clearfix u-container-align-center  " id="carousel_d430" style={{ paddingTop:"20px", paddingLeft:"40px", paddingRight:"40px", marginDown:"0px", marginTop:"0px"}}>
-        <div className="u-clearfix u-sheet u-sheet-1">
+      <section  className=" u-align-center u-clearfix u-container-align-center  u-section-4 " id="carousel_d430" style={{ paddingTop:"0px", paddingLeft:"15px", paddingRight:"15px", paddingDown:"10px", paddingTop:"40px"}}>
           <h5 className="u-align-center u-text u-text-1" data-animation-name="customAnimationIn" data-animation-duration="1500" data-animation-delay="750"><small>Nos produits</small></h5>
           <p className="u-align-center u-text u-text-2" data-animation-name="customAnimationIn" data-animation-duration="1500"><small>Le Sénégal offre une richesse de produits locaux reflétant sa diversité culturelle et géographique. Voici un résumé de certains des produits locaux clés du Sénégal</small></p>
-          <div className="u-expanded-width u-list u-list-1">
-            <div className="u-repeater u-repeater-1">
+            <div className="pt-4 pb-5 u-repeater u-repeater-1">
               {
-                  Data.map((data)=><Photopanel2 title={data[1]} image={data[0]} price='$19.99'/>)
+                  Data.map((data)=><Photopanel2 title={data[1]} image={data[0]}/>)
                   
               }
             </div>
-          </div>
-        </div>
       </section>
+      
   )
 }
 
