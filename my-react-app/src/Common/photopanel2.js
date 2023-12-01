7eb55f6264b94241460bb98c3e88ce713013fd19
Marius_DIATTA <marius@kayfo.games>
2023-12-01 10:36:26 +0100

    panel

--- my-react-app/src/Common/photopanel2.js
+++ my-react-app/src/Common/photopanel2.js
@@ -1,5 +1,6 @@
 import { Link } from 'react-router-dom';
 import { useState } from "react";
+import  './../style.css'
 
 const couleur ="rgb(248,245,244)";
 const couleurInit ="white";
@@ -9,10 +10,10 @@ const Photopanel2 = (props)=>{
     const [couleurImageActivate, setCouleurImage] =useState(false)
 
     return(
-        <div className='card p-0 m-2' onMouseOut={()=> setCouleurImage(false)} onMouseOver={()=> setCouleurImage(true)} style={{backgroundColor:couleurImageActivate?couleur:couleurInit, width:'310px', height:'350px'}}>
+        <div className='card ' onMouseOut={()=> setCouleurImage(false)} onMouseOver={()=> setCouleurImage(true)} style={{backgroundColor:couleurImageActivate?couleur:couleurInit, width:'310px', height:'350px', paddingBottom:""}}>
          <img src={props.image} className="card-img-top" alt="..." width={100} height={180}/>
          <h6>{props.title}</h6>
-         <p className="price p-0 m-0">{props.price}</p>
+         <p className="price">{props.price}</p>
          <Link to='/aboutUser'><small>Savoir plus!</small>  </Link><Link to='/commande' style={{color:"black"}}><small>Passez une commade.</small> </Link>  
        </div> 
     )
