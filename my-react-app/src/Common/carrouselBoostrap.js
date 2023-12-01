1fb0ba37a26924581e965087283fde14b501674c
Marius_DIATTA <marius@kayfo.games>
2023-12-01 02:47:44 +0100

    carroussel done

--- my-react-app/src/Common/carrouselBoostrap.js
+++ my-react-app/src/Common/carrouselBoostrap.js
@@ -3,23 +3,23 @@ import Carousel from 'react-bootstrap/Carousel';
 
 const Carrous =() =>{
     return(
-    <Carousel>
-      <Carousel.Item interval={1000} style={{border:"0px"}}>
-        <img src={im} style={{height:"300px", weight:"100%"}}/>
+    <Carousel className="u-align-center">
+      <Carousel.Item interval={1010} style={{border:"0px"}}>
+        <img src={im} width={window.innerWidth} height={window.innerHeight/2}/>
         <Carousel.Caption>
           <h3>First slide label</h3>
           <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
         </Carousel.Caption>
       </Carousel.Item>
-      <Carousel.Item interval={1000} style={{border:"0px"}}>
-        <img src={im} style={{height:"300px", weight:"100%"}}/>
+      <Carousel.Item interval={2000} style={{border:"0px"}}>
+        <img src={im} width={window.innerWidth}  height={window.innerHeight/2}/>
         <Carousel.Caption>
           <h3>Second slide label</h3>
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
         </Carousel.Caption>
       </Carousel.Item>
-      <Carousel.Item interval={1000} style={{border:"0px"}}>
-        <img src={im}  style={{height:"300px", weight:"100%"}}/>
+      <Carousel.Item interval={2000} style={{border:"0px"}}>
+        <img src={im}  width={window.innerWidth}  height={window.innerHeight/2}/>
         <Carousel.Caption>
           <h3>Third slide label</h3>
           <p>
