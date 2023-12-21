7af53e5cc9680be7d04f69113cf01d41813c8ff7
Marius_DIATTA <marius@kayfo.games>
2023-12-21 02:03:52 +0100

    done

--- my-react-app/src/Common/sectionU.js
+++ my-react-app/src/Common/sectionU.js
@@ -19,11 +19,6 @@ function SectionU(){
       <section  className=" u-align-center u-clearfix u-container-align-center  u-section-4 " id="carousel_d430" >
           <h5 className="u-align-center u-text " data-animation-name="customAnimationIn" data-animation-duration="1500" data-animation-delay="750"><strong>Nos produits</strong></h5>
           <p className="u-align-center u-text " data-animation-name="customAnimationIn" data-animation-duration="1500"><small>Le Sénégal offre une richesse de produits locaux reflétant sa diversité culturelle et géographique. Voici un résumé de certains des produits locaux clés du Sénégal.</small></p>
-            {/* <div className="pt-3 pb-5 u-repeater u-repeater-1"> */}
-              {/* {
-                  Data.map((data)=><Photopanel2 title={data[1]} image={data[0]}/>) 
-              } */}
-
               <div className="container" style={{paddingLeft:'5%', paddingRight:'6%'}}>   
                 <Row xs={1} md={3} className="g-3">
                     {
@@ -37,8 +32,6 @@ function SectionU(){
                     }
                 </Row>
               </div>  
-
-            {/* </div> */}
       </section>
       
   )
