549f09a522463093ba679102d541074be9455da1
Marius_DIATTA <marius@kayfo.games>
2023-12-22 03:37:03 +0100

    page admin done

--- my-react-app/src/Common/pageAdmin.js
+++ my-react-app/src/Common/pageAdmin.js
@@ -11,11 +11,15 @@ const Admin =(props)=>{
             {
                 (props.valueHeaderState)? 
 
-                <div className="row flex-row"> 
+                <div className="row flex-row vert_menu_footer"> 
                     <VerticalMenu/>
-                    <div className="col-lg-10 ">
-                        <Section/>
-                        <Footer/>
+                    <div className="col-lg-10 class_pad">
+                        <div className="row">
+                            <Section/>
+                        </div>
+                    </div>
+                    <div className="pl-5 container class_footer">
+                            <Footer/>
                     </div>  
                 </div>
 
--- my-react-app/src/Common/verticalMenu.js
+++ my-react-app/src/Common/verticalMenu.js
@@ -16,49 +16,49 @@ const VerticalMenu=()=>{
     const [messageActivate, setMessageActivate]=useState(false)
 
     return (
-        <>  
-            <nav  className='style_css  col-lg-2' style={{backgroundColor:'white'}}>
-                <ul className="flex-row sticky-bottom ">
-                    <li>  <button className='btn' onClick={()=>messageActivate?setMessageActivate(false):setMessageActivate(true)} onMouseOver={()=>setCouleurMessage(couleurBlue)} onMouseOut={()=>setCouleurMessage(couleurWhite)} style={couleurMessage}>Messages</button></li>
-                    { 
-                        (messageActivate)?
-                        <nav style={{backgroundColor:'white'}}>
+
+        <nav className='sticky-top u-align-center style_css col-lg-2' >
+            <ul className="">
+                <li>  <button className='code_u btn' onClick={()=>messageActivate?setMessageActivate(false):setMessageActivate(true)} onMouseOver={()=>setCouleurMessage(couleurBlue)} onMouseOut={()=>setCouleurMessage(couleurWhite)} style={couleurMessage}><small>Messages</small></button></li>
+                { 
+                    (messageActivate)?
+                    <nav style={{backgroundColor:'white'}}>
+                    <ul >
+                        <li> <Link to="/" ><small>Nouveaux Messages</small></Link></li>
+                        <li> <Link to="#" ><small>Anciencs Messages</small></Link></li>
+                    </ul>
+                    </nav>: <nav> </nav>
+            
+                }
+            </ul>
+            <ul>
+                <li > <button className='code_u btn' onClick={()=>adherantActivate?setAdherantActivate(false):setAdherantActivate(true)}  onMouseOver={()=>setcouleurAdherant(couleurBlue)} onMouseOut={()=>setcouleurAdherant(couleurWhite)} style={couleurAdherant}><small>Adherants</small></button></li>
+                {  
+                    (adherantActivate)?
+                    <nav style={{backgroundColor:'white'}}>
                         <ul >
-                            <li> <Link to="/" ><small>Nouveaux Messages</small></Link></li>
-                            <li> <Link to="#" ><small>Anciencs Messages</small></Link></li>
+                            <li> <Link to="#" ><small>Nouveaux Adhérants</small></Link></li>
+                            <li> <Link to="#" ><small>Anciens Adhérants</small></Link></li>
                         </ul>
-                        </nav>: <nav> </nav>
-                
-                    }
-                </ul>
-                <ul className="  flex-row " >
-                    <li > <button className='btn' onClick={()=>adherantActivate?setAdherantActivate(false):setAdherantActivate(true)}  onMouseOver={()=>setcouleurAdherant(couleurBlue)} onMouseOut={()=>setcouleurAdherant(couleurWhite)} style={couleurAdherant}>Adherants</button></li>
-                    {  
-                        (adherantActivate)?
-                        <nav style={{backgroundColor:'white'}}>
-                            <ul >
-                                <li> <Link to="#" ><small>Nouveaux Adhérants</small></Link></li>
-                                <li> <Link to="#" ><small>Anciens Adhérants</small></Link></li>
-                            </ul>
-                        </nav>:<nav></nav>
-                    }
-                 </ul>
-                 <ul className="  flex-row ">
-                    <li > <button className='btn' onClick={()=>commandeActivate?setCommandeActivate(false):setCommandeActivate(true)} onMouseOver={()=>setCouleurCommande(couleurBlue)} onMouseOut={()=>setCouleurCommande(couleurWhite)}  style={couleurCommande}>Commandes</button></li>
-                    {                        
-                        (commandeActivate)?
-                        <nav style={{backgroundColor:'white'}}>
-                            <ul>
-                                <li> <Link to="#" ><small>Commandes réalisées</small></Link></li>
-                                <li> <Link to="#" ><small>Commandes programées</small></Link></li>
-                                <li> <Link to="#" ><small>Commandes en cours</small></Link></li>
-                                <li> <Link to="#" ><small>Commandes prêtes</small></Link></li>
-                            </ul>
-                        </nav>: <nav></nav>
-                    }
-                </ul>
-            </nav>
-        </>
+                    </nav>:<nav></nav>
+                }
+            </ul>
+            <ul>
+                <li > <button className='code_u btn' onClick={()=>commandeActivate?setCommandeActivate(false):setCommandeActivate(true)} onMouseOver={()=>setCouleurCommande(couleurBlue)} onMouseOut={()=>setCouleurCommande(couleurWhite)}  style={couleurCommande}><small>Commandes</small></button></li>
+                {                        
+                    (commandeActivate)?
+                    <nav style={{backgroundColor:'white'}}>
+                        <ul>
+                            <li> <Link to="#" ><small>Commandes réalisées</small></Link></li>
+                            <li> <Link to="#" ><small>Commandes programées</small></Link></li>
+                            <li> <Link to="#" ><small>Commandes en cours</small></Link></li>
+                            <li> <Link to="#" ><small>Commandes prêtes</small></Link></li>
+                        </ul>
+                    </nav>: <nav></nav>
+                } 
+            </ul>
+        </nav>
+        
     )
 }
 
