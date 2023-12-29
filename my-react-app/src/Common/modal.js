cfbdd23afe27808f87ff5c0f372d939932d753e3
Marius_DIATTA <marius@kayfo.games>
2023-12-29 03:40:08 +0100

    page admin done

--- my-react-app/src/Common/modal.js
+++ my-react-app/src/Common/modal.js
@@ -32,36 +32,30 @@ function ModalPop() {
         </Modal.Header>
 
         <Modal.Body>
-            <div className='u-align-center off_classe'>
+            <div className='u-align-center off_classe' style={{position:'sticky'}}>
                 <ul>
-                    <li>  <button className='code_u btn' onClick={()=>messageActivate?setMessageActivate(false):setMessageActivate(true)} onMouseOver={()=>setCouleurMessage(couleurBlue)} onMouseOut={()=>setCouleurMessage(couleurWhite)} style={couleurMessage}><small>Messages</small></button></li>
-                    { 
-                        (messageActivate)?
+                    <li><small>Messages</small></li>
+
                         <nav style={{backgroundColor:'white'}}>
                         <ul >
                             <li> <Link to="/" ><small>Nouveaux Messages</small></Link></li>
                             <li> <Link to="#" ><small>Anciencs Messages</small></Link></li>
                         </ul>
-                        </nav>: <nav> </nav>
-                
-                    }
+                        </nav>
                 </ul>
                 <ul>
-                    <li > <button className='code_u btn' onClick={()=>adherantActivate?setAdherantActivate(false):setAdherantActivate(true)}  onMouseOver={()=>setcouleurAdherant(couleurBlue)} onMouseOut={()=>setcouleurAdherant(couleurWhite)} style={couleurAdherant}><small>Adherants</small></button></li>
-                    {  
-                        (adherantActivate)?
+                    <li ><small>Adherants</small></li>
+
                         <nav style={{backgroundColor:'white'}}>
                             <ul >
                                 <li> <Link to="#" ><small>Nouveaux Adhérants</small></Link></li>
                                 <li> <Link to="#" ><small>Anciens Adhérants</small></Link></li>
                             </ul>
-                        </nav>:<nav></nav>
-                    }
+                        </nav>
                 </ul>
                 <ul>
-                    <li > <button className='code_u btn' onClick={()=>commandeActivate?setCommandeActivate(false):setCommandeActivate(true)} onMouseOver={()=>setCouleurCommande(couleurBlue)} onMouseOut={()=>setCouleurCommande(couleurWhite)}  style={couleurCommande}><small>Commandes</small></button></li>
-                    {                        
-                        (commandeActivate)?
+                    <li ><small>Commandes</small></li>
+
                         <nav style={{backgroundColor:'white'}}>
                             <ul>
                                 <li> <Link to="#" ><small>Commandes réalisées</small></Link></li>
@@ -69,8 +63,7 @@ function ModalPop() {
                                 <li> <Link to="#" ><small>Commandes en cours</small></Link></li>
                                 <li> <Link to="#" ><small>Commandes prêtes</small></Link></li>
                             </ul>
-                        </nav>: <nav></nav>
-                    } 
+                        </nav>
                 </ul>
             </div>
         </Modal.Body>
