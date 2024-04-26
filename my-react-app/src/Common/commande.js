cf3e4485af491492c967ec27e9dc68febce6b261
Marius_DIATTA <marius@kayfo.games>
2024-04-26 14:37:45 +0200

    page commande done

--- my-react-app/src/Common/commande.js
+++ my-react-app/src/Common/commande.js
@@ -8,8 +8,7 @@ import Image from 'react-bootstrap/Image';
 import Button from 'react-bootstrap/Button';
 
 
-const Commande =()=>{
-    
+   const Commande =()=>{
    // Tableaux d'origine et de destination
     const [sourceArray, setSourceArray] = useState( [
         {prod:0,  lab:'Ford',prix:'100', qual:'bon'},
@@ -21,6 +20,19 @@ const Commande =()=>{
     ]);
 
     const [destinationArray, setDestinationArray] = useState([]);
+
+    
+    const handleSubmit=(e)=>{
+
+        e.preventDefault();
+
+        if (destinationArray.length!==0) {
+            document.getElementById('monid').innerHTML="Bravo!!! Vous avez fait des choix";
+        }else{
+            document.getElementById('mon_id').innerHTML="Il y n'a pas de choix";
+        }
+
+    };
 
     // Fonction pour sélectionner un élément de la source et l'ajouter à la destination
     const selectAndAddToDestination = (index) => {
@@ -36,6 +48,7 @@ const Commande =()=>{
     };
 
     const supprimerElement= (index) => {
+        
         const selectedElementToDelete=destinationArray[index];
         const updateDestinationArray=destinationArray.filter((_,i)=>i!==index);
         setDestinationArray(updateDestinationArray);
@@ -56,9 +69,7 @@ const Commande =()=>{
                         <div className="u-align-left u-container-style u-layout-cell u-left-cell u-size-30 u-layout-cell-1" data-animation-name="customAnimationIn" data-animation-duration="1250" data-animation-delay="500">
                             
                             <div className="u-container-layout u-container-layout-1">
-
                                 <Image src={im} alt="" className="u-align-left u-expanded-width u-image " width={200} height={300} rounded/>
-                            
                             </div>
 
                         </div>
@@ -83,10 +94,8 @@ const Commande =()=>{
 
                                     <tbody>
                                         {
-                                        sourceArray.map((car, index) =><ProduitSelected prod={car.prod}  lab={car.lab} prix={car.prix} qual={car.qual} onClick={() => selectAndAddToDestination(index)} />)
-            
-                                        }
-                                        
+                                            sourceArray.map((car, index) =><ProduitSelected prod={car.prod}  lab={car.lab} prix={car.prix} qual={car.qual} onClick={() => selectAndAddToDestination(index)} />)
+                                        }  
                                     </tbody>
 
                                 </Table>
@@ -104,7 +113,7 @@ const Commande =()=>{
                         
                         <h5 style={{color:'grey'}}> Enregistrer vos données pour valider l'opération.</h5>
 
-                        <form>
+                        <form onSubmit={handleSubmit}>
                             <FloatingLabel controlId="floatingPassword2" label="Votre nom" className=' mb-3'>
                                 <Form.Control  id="floatingPassword2" name="nom" value=""  type="nom" placeholder="Enter a valid email address"  maxLength="30" className="u-align-center u-input" />
                             </FloatingLabel>
@@ -150,8 +159,9 @@ const Commande =()=>{
                                 </Table >
                                 
                             </div>
-
-                            <button className='btn btn-success p-2 m-2'> Valider</button >
+                            <p id="mon_id" style={{color:'red'}}></p>
+                            <p id="monid" style={{color:'grenn'}}></p>
+                            <button type="submit" className='btn btn-success p-2 m-2'> Valider</button >
                         
                         </form> 
                     
